9d76945c7f31400cdbcddeb49a46a0a1991a610e
Akinaru <maxime.utchimata@gmail.com>
2024-04-04 14:19:19 +0200

    matchs club

--- src/components/MatchClubComponent.vue
+++ src/components/MatchClubComponent.vue
@@ -1,15 +1,24 @@
 <template>
-    <div v-if="match" class="bg-base-200 m-5 w-2/3 p-5 flex items-center justify-center">
+    <div v-if="match" :class="{
+    'bg-green-200': (match.clubDomicile === clubId && match.matchScoreDomicile > match.matchScoreExterieur),
+    'bg-red-200': (match.clubDomicile === clubId && match.matchScoreDomicile < match.matchScoreExterieur),
+    'bg-base-200': (match.matchScoreDomicile === match.matchScoreExterieur),
+    'bg-green-200': (match.clubExterieur === clubId && match.matchScoreExterieur > match.matchScoreDomicile),
+    'bg-red-200': (match.clubExterieur === clubId && match.matchScoreExterieur < match.matchScoreDomicile)
+}" class="m-5 w-2/3 p-5 flex items-center justify-center">
         <div class="w-full">
 
             <p>{{ match.matchDate }}</p>
             <div class="m-5 flex items-center justify-center text-2xl *:mx-2">
-                <div class="flex items-center justify-center flex-col">
-                    <p class="w-72 flex items-center justify-center">{{ clubDomi.clubNom }}</p>
+                <div class="w-96 flex items-center justify-center flex-col">
+                    <p class="flex items-center justify-center font-bold">{{ clubDomi.clubNom }}</p>
                     <p>{{ clubDomi.clubInitiales }}</p>
                 </div>
-                <p class="text-3xl font-bold w-48 flex items-center justify-center"> {{ match.matchScoreDomicile }} - {{ match.matchScoreExterieur }} </p>
-                <p class="w-72 flex items-center justify-center">{{ clubExt.clubNom }}</p>
+                <p class="text-4xl font-bold w-48 flex items-center justify-center"> {{ match.matchScoreDomicile }} - {{ match.matchScoreExterieur }} </p>
+                <div class="w-96 flex items-center justify-center flex-col">
+                    <p class="flex items-center justify-center font-bold">{{ clubExt.clubNom }}</p>
+                    <p>{{ clubExt.clubInitiales }}</p>
+                </div>
             </div>
         </div>
     </div>
@@ -26,7 +35,6 @@
     const clubDomi = ref([]);
     const clubExt = ref([]);
     async function fetchAll(){
-
         await getRequest(clubExt, 'https://apififa2.azurewebsites.net/api/club/getbyid/'+props.match.clubExterieurId)
         await getRequest(clubDomi, 'https://apififa2.azurewebsites.net/api/club/getbyid/'+props.match.clubDomicileId)
     }
--- src/pages/club.vue
+++ src/pages/club.vue
@@ -1,14 +1,29 @@
 <template>
     <div>
-        <div v-if="club && club.clubId">
-            {{ club }}
-            <p class="text-2xl font-bold">{{ club.clubNom }}</p>
-            <p>{{ club.clubInitiales }}</p>
-
-            <p>Derniers match:</p>
-            <p>Domiciles:</p>
-            <div class="flex items-center justify-center flex-col">
-                <MatchClubComponent v-for="(match,index) in club.matchesDomicile" :key="index" :match="match"></MatchClubComponent>
+        <div v-if="club && club.clubId" class="m-10">
+            <div class="flex items-end justify-start">
+                <p class="text-5xl font-bold">{{ club.clubNom }}</p>
+                <p class="text-xl flex items-end mx-2">({{ club.clubInitiales }})</p>
+            </div>
+            
+            <div class="my-10">
+                <p class="text-3xl">Matchs:</p>
+                <div class="divider"></div>
+                <p class="text-2xl">Domiciles:</p>
+                <div class="flex items-center justify-center flex-col">
+                    <MatchClubComponent v-for="(match, index) in displayedMatchesDomi" :key="index" :match="match"></MatchClubComponent>
+                </div>
+                <div class="flex justify-center items-center m-10">
+                    <button @click="loadMoreMatchesDomi" v-if="displayedMatchesDomi.length < club.matchesDomicile.length" class="btn btn-primary">Voir plus</button>
+                </div>
+                <div class="divider"></div>
+                <p class="text-2xl">Exterieur:</p>
+                <div class="flex items-center justify-center flex-col">
+                    <MatchClubComponent v-for="(match,index) in displayedMatchesExt" :key="index" :match="match"></MatchClubComponent>
+                </div>
+                <div class="flex justify-center items-center m-10">
+                    <button @click="loadMoreMatchesExt" v-if="displayedMatchesExt.length < club.matchesExterieur.length" class="btn btn-primary">Voir plus</button>
+                </div>
             </div>
         </div>
         <div v-else class="flex justify-center items-center h-screen">
@@ -21,24 +36,56 @@
     import { useRouter, useRoute } from 'vue-router';
     import { onMounted, ref } from 'vue';
     import { getRequest } from '../composable/httpRequests';
-
-    import MatchClubComponent from '../components/MatchClubComponent.vue'
+    import MatchClubComponent from '../components/MatchClubComponent.vue';
 
     const route = useRoute();
     const router = useRouter();
-    function retour (){
-        router.back()
-    }
     const clubId = route.params.id;
 
     const club = ref([]);
+    const displayedMatchesDomi = ref([]);
+    const displayedMatchesExt = ref([]);
+    const matchesPerPage = 5;
+    let startIndexDomi = 0;
+    let startIndexExt = 0;
 
-
-    async function fetchAll(){
-        await getRequest(club, 'https://apififa2.azurewebsites.net/api/club/getbyid/'+clubId)
+    async function fetchAll() {
+        await getRequest(club, 'https://apififa2.azurewebsites.net/api/club/getbyid/' + clubId);
+        sortMatchesByDate(club.value.matchesDomicile);
+        sortMatchesByDate(club.value.matchesExterieur);
+        updateDisplayedMatchesDomi();
+        updateDisplayedMatchesExt();
     }
 
-    onMounted(fetchAll)
+    function loadMoreMatchesDomi() {
+        startIndexDomi += matchesPerPage;
+        updateDisplayedMatchesDomi();
+    }
+    function loadMoreMatchesExt() {
+        startIndexExt += matchesPerPage;
+        updateDisplayedMatchesExt();
+    }
 
+    function updateDisplayedMatchesDomi() {
+        displayedMatchesDomi.value = club.value.matchesDomicile.slice(0, startIndexDomi + matchesPerPage);
+    }
+    function updateDisplayedMatchesExt() {
+        displayedMatchesExt.value = club.value.matchesExterieur.slice(0, startIndexExt + matchesPerPage);
+    }
 
-    </script>
+    function sortMatchesByDate(matches) {
+    matches.sort((a, b) => {
+        // Convertir les dates en objets Date pour faciliter la comparaison
+        const dateA = new Date(a.matchDate);
+        const dateB = new Date(b.matchDate);
+        // Comparer les dates pour le tri (du plus récent au plus ancien)
+        return dateB - dateA;
+    });
+}
+
+    function retour() {
+        router.back();
+    }
+
+    onMounted(fetchAll);
+</script>
