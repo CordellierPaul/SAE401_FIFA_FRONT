61ae53b872b99efdb346296956fae6349866625c
Paul Cordellier <cordellierp@gmail.com>
2024-04-07 16:31:38 +0200

    Modification de base de l'utilisateur

--- src/pages/parametresCompte.vue
+++ src/pages/parametresCompte.vue
@@ -10,6 +10,11 @@ const donneesCompte = ref()
 const deleteButtonClass = "block bg-red-500 text-white hover:bg-white hover:text-black border border-red-500 rounded-lg py-2 px-4 duration-75 my-2"
 const basicButtonClass = "block bg-gray-500 text-white hover:bg-white hover:text-black border border-gray-500 rounded-lg py-2 px-4 duration-75 my-2"
 
+const readonlyCompteDataClass = "inline-block py-1.5"
+const inputClass = "inline p-1 w-max border border-gray-200 rounded-lg text-s focus:ring-blue-500 focus:border-blue-500 border-gray-600 focus:ring-blue-500 focus:border-blue-500"
+
+var modificationEnCours = ref(false)
+
 async function fetchCompteData() {
 
     const response = await fetch("https://apififa2.azurewebsites.net/api/compte/getbyid/" + compteStore.compteId, {
@@ -21,9 +26,37 @@ async function fetchCompteData() {
     })
 
     donneesCompte.value = await response.json()
+
+    donneesCompte.value.utilisateurCompte.utilisateurId = undefined
 }
 
 onMounted(fetchCompteData)
+
+function modifierCompte() {
+    modificationEnCours.value = true
+}
+
+async function enregistrerModifications() {
+
+    const response = await fetch("https://apififa2.azurewebsites.net/api/compte/" + compteStore.compteId, {
+        method: "PUT",
+        headers: {
+            "Authorization": `Bearer ${compteStore.token}`, 
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(donneesCompte.value)
+    })
+    
+    await fetchCompteData()
+
+    modificationEnCours.value = false
+}
+
+async function annulerModifications() {
+    donneesCompte.value = undefined
+    await fetchCompteData()
+    modificationEnCours.value = false
+}
 
 async function supprimerCompte() {
 
@@ -45,18 +78,34 @@ async function supprimerCompte() {
 
 <p class="flex justify-center items-center m-12 text-3xl font-bold">Paramètres du compte</p>
 
-<div v-if="donneesCompte" class="container sm:mx-auto px-5 py-2">
+<div v-if="donneesCompte" class="container mx-auto max-w-7xl px-4 pb-8">
 
     <p class="text-2xl font-semibold">Informations générales :</p>
     
-    <p>Nom d'utilisateur (login) : {{ donneesCompte.comptelogin }}</p>
-    <p>Email : {{ donneesCompte.compteEmail }}</p>
-    <p>Nom : {{ donneesCompte.utilisateurCompte.utilisateurNomAcheteur }}</p>
-    <p>Prénom : {{ donneesCompte.utilisateurCompte.prenomUtilisateur }}</p>
+    <div>
+        <p class="inline">Nom d'utilisateur (login) : </p>
+        <input v-if="modificationEnCours" id="comptelogin" :class="inputClass" v-model="donneesCompte.comptelogin">
+        <p v-else :class="readonlyCompteDataClass" id="comptelogin">{{ donneesCompte.comptelogin }}</p>
+    </div>
+    <div>
+        <p class="inline">Email : </p>
+        <input v-if="modificationEnCours" id="compteEmail" :class="inputClass" v-model="donneesCompte.compteEmail">
+        <p v-else :class="readonlyCompteDataClass" id="compteEmail">{{ donneesCompte.compteEmail }}</p>
+    </div>
+    <div>
+        <p class="inline">Nom : </p>
+        <input v-if="modificationEnCours" id="utilisateurNomAcheteur" :class="inputClass" v-model="donneesCompte.utilisateurCompte.utilisateurNomAcheteur">
+        <p v-else :class="readonlyCompteDataClass" id="utilisateurNomAcheteur">{{ donneesCompte.utilisateurCompte.utilisateurNomAcheteur }}</p>
+    </div>
+    <div>
+        <p class="inline">Prénom : </p>
+        <input v-if="modificationEnCours" id="prenomUtilisateur" :class="inputClass" v-model="donneesCompte.utilisateurCompte.prenomUtilisateur">
+        <p v-else :class="readonlyCompteDataClass" id="prenomUtilisateur">{{ donneesCompte.utilisateurCompte.prenomUtilisateur }}</p>
+    </div>
 
-    <p class="text-2xl font-semibold">Commandes :</p>
-    <p v-if="donneesCompte.utilisateurCompte.commandesUtilisateur">Vous avez des commandes !</p>
-    <p v-else>Vous n'avez pas de commandes pour le moment</p>
+    <button v-if="modificationEnCours" :class="basicButtonClass" @click="enregistrerModifications">Enregister</button>
+    <button v-if="modificationEnCours" :class="basicButtonClass" @click="annulerModifications">Annuler</button>
+    <button v-else :class="basicButtonClass" @click="modifierCompte">Modifier le compte</button>
 
     <button :class="basicButtonClass">  <!-- Sans utilisation du bouton le css fait des trucs bizarres -->
         <RouterLink :to="{name: 'donnees-bancaires'}">Modifier mes données bancaires</RouterLink>
@@ -66,6 +115,6 @@ async function supprimerCompte() {
 
 </div>
 
-<p v-else>Chargement...</p>
+<p v-else class="text-xl mx-auto max-w-7xl px-4 pb-8">Chargement...</p>
 
 </template>
