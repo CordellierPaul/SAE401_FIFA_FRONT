451149102f5b5c34216ffe4213a13b7993ddf114
Akinaru <maxime.utchimata@gmail.com>
2024-04-09 10:05:43 +0200

    loading while fetching datas

--- src/pages/articles.vue
+++ src/pages/articles.vue
@@ -1,18 +1,24 @@
 <template>
     <div class="min-h-screen">
-        <div class="sticky top-20 z-[5] bg-secondary p-4 flex  items-center text-white min-h-20" >
+        <div v-if="articles">
 
-            <div class="text-sm breadcrumbs hidden lg:block">
-                <ul>
-                    <li><RouterLink :to="{name: 'index'}" class="hover:opacity-50 hover:cursor-pointer">FIFA</RouterLink></li> 
-                    <li>Articles</li>
-                </ul>
+            <div class="sticky top-20 z-[5] bg-secondary p-4 flex  items-center text-white min-h-20" >
+                
+                <div class="text-sm breadcrumbs hidden lg:block">
+                    <ul>
+                        <li><RouterLink :to="{name: 'index'}" class="hover:opacity-50 hover:cursor-pointer">FIFA</RouterLink></li> 
+                        <li>Articles</li>
+                    </ul>
+                </div>
+            </div>
+            <p class="flex justify-center items-center m-12 text-3xl font-bold">Articles</p>
+            <div class="m-5 *:my-8">
+                
+                <ArticleComponent v-if="articles" v-for="(article, index) in articles" key="index" :article="article"></ArticleComponent>
             </div>
         </div>
-        <p class="flex justify-center items-center m-12 text-3xl font-bold">Articles</p>
-        <div class="m-5 *:my-8">
-
-            <ArticleComponent v-if="articles" v-for="(article, index) in articles" key="index" :article="article"></ArticleComponent>
+        <div v-else class="flex justify-center items-center h-screen">
+            <span class="loading loading-spinner loading-lg"></span>
         </div>
 
     </div>
