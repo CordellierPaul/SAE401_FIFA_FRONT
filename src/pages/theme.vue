4e24de83b22575af05358a2ce5ae6e2a52c005bf
anna.kompaniets <anna.kompaniets@etu.univ-smb.fr>
2024-04-09 09:35:07 +0000

    voter une seule fois + performant

--- src/pages/theme.vue
+++ src/pages/theme.vue
@@ -9,6 +9,8 @@ const compteStore = useCompteStore();
 const themes = ref([]);
 const router = useRouter();
 
+const votes = ref([]);
+
 async function fetchThemes() {
   try {
     const response = await fetch('https://apififa2.azurewebsites.net/api/Theme', {
@@ -22,11 +24,48 @@ async function fetchThemes() {
   }
 }
 
-function redirectToVotePage(themeId) {
-  router.push({ name: 'vote', query: { id: themeId } });
+async function fetchVotes() {
+        try {
+        const responseVote = await fetch(`https://apififa2.azurewebsites.net/api/Vote`, {
+            method: 'GET',
+            mode: 'cors'
+        });
+
+        votes.value = await responseVote.json();
+
+
+        } catch (error) {
+        console.error('Erreur lors de la récupération des votes :', error);
+        }
+
+       
+    }
+
+  function redirectToVotePage(themeId) {
+    router.push({ name: 'vote', query: { id: themeId } });
+  }
+
+  function redirectToConnexion() {
+    router.push({ name: 'login' });
+  }
+
+  onMounted(async () => {
+        await fetchThemes();
+        await fetchVotes();
+      })
+      
+  function dejaVote(themeId) {
+    for (var vote of votes.value) {
+        if (
+            vote.utilisateurId === parseInt(compteStore.utilisateur[0].utilisateurId, 10) &&
+            vote.themeId === themeId
+        ) {
+            return true;
+        }
+    }
+    return false;
 }
 
-onMounted(fetchThemes);
 </script>
 
 
@@ -49,15 +88,21 @@ onMounted(fetchThemes);
     <div class="grid grid-cols-3 gap-4 place-content-stretch ">
       <div v-for="theme in themes" :key="theme.themeId" class="collapse collapse-arrow bg-base-300 p-5">
         <p class="flex justify-center font-bold p-12"> {{ theme.themeLibelle }} </p>
-        <template v-if="compteStore.isConnected">
-          <!-- Utilisateur connecté -->
-          <button class="btn btn-outline btn-primary" @click="redirectToVotePage(theme.themeId)">
-            Voter
-          </button>
+        <template v-if="compteStore.isConnected && !dejaVote(theme.themeId)">
+            <!-- Utilisateur connecté -->
+            <button class="btn btn-outline btn-primary" @click="redirectToVotePage(theme.themeId)">
+                Voter
+            </button>
         </template>
-        <template v-else>
-          <!-- Utilisateur non connecté -->
-          <button class="btn btn-outline btn-primary">Connectez-vous pour voter</button>
+        <template v-if="compteStore.isConnected && dejaVote(theme.themeId)">
+            <!-- Déjà Voté -->
+            <button class="btn btn-outline btn-primary">
+                Vous ne pouvez plus voter
+            </button>
+        </template>
+        <template v-if="!compteStore.isConnected">
+            <!-- Utilisateur non connecté -->
+            <button class="btn btn-outline btn-primary" @click="redirectToConnexion()" >Connectez-vous pour voter</button>
         </template>
       </div>
     </div>
