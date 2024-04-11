1441536187a5a2f4976d5f272c3fb3935359b22e
Paul Cordellier <cordellierp@gmail.com>
2024-04-11 08:43:01 +0200

    dernier commit

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:5179',
+    baseUrl: 'http://localhost:5173/~sa13/',
     testIsolation: false
   }
 })
